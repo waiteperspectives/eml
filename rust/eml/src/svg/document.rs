8c20ad86da6da980f5ba645d25eecf1c2b540220
Zach Waite <zach@waiteperspectives.com>
2022-09-12 17:23:52 -0400

    [IMP] eml: update tests

--- rust/eml/src/svg/document.rs
+++ rust/eml/src/svg/document.rs
@@ -113,12 +113,10 @@ impl SvgDocument {
 mod tests {
     use super::*;
     use crate::utils::newid;
-    use std::fs::File;
-    use std::io::Write;
 
     #[test]
     fn test_document() {
-        let expected = "<svg xmlns='http://www.w3.org/2000/svg' width='1650' height='1350'><g id='swimlane' stroke='black' stroke-width='3' ><line id='swimlane_top' x1='0' y1='450' x2='1650' y2='450' /><line id='swimlane_bottom' x1='0' y1='900' x2='1650' y2='900' /></g><path d='M 450 225 Q 750 225 750 600' stroke='black' stroke-width='2' fill='none' /><path d='M 750 750 Q 750 1275 1050 1275' stroke='black' stroke-width='2' fill='none' /><rect id='form1' stroke='black' stroke-width='2' x='150' y='150' height='150' width='300' fill='#ffffff' /><text transform='translate(162.5 162.5)' x='0' y='0' style='white-space: pre;' font-family='monospace' ><tspan x='0' dy='1rem' font-size='larger' font-weight='bold'>form1</tspan><tspan x='0' dy='1rem'>==========</tspan><tspan x='0' dy='1rem'>Test Form</tspan></text><rect id='cmd1' stroke='black' stroke-width='2' x='600' y='600' height='150' width='300' fill='#60b3f7' /><text transform='translate(612.5 612.5)' x='0' y='0' style='white-space: pre;' font-family='monospace' ><tspan x='0' dy='1rem' font-size='larger' font-weight='bold'>cmd1</tspan><tspan x='0' dy='1rem'>==========</tspan><tspan x='0' dy='1rem'>Test Cmd</tspan></text><rect id='evt1' stroke='black' stroke-width='2' x='1050' y='1200' height='150' width='300' fill='#f7a660' /><text transform='translate(1062.5 1212.5)' x='0' y='0' style='white-space: pre;' font-family='monospace' ><tspan x='0' dy='1rem' font-size='larger' font-weight='bold'>evt1</tspan><tspan x='0' dy='1rem'>==========</tspan><tspan x='0' dy='1rem'>Test Event</tspan><tspan x='0' dy='1rem'>+ foo: str</tspan><tspan x='0' dy='1rem'>+ bar: str</tspan></text></svg>".to_string();
+        let expected = "<svg xmlns='http://www.w3.org/2000/svg' width='1650' height='1350'><defs><marker id='triangle' viewBox='0 0 10 10' refX='0' refY='5' markerUnits='strokeWidth' markerWidth='10' markerHeight='8' orient='auto'><path d='M 0 0 L 10 5 L 0 10 z' /></marker></defs><g id='swimlane' stroke='black' stroke-width='3' ><line id='swimlane_top' x1='0' y1='450' x2='1650' y2='450' /><line id='swimlane_bottom' x1='0' y1='900' x2='1650' y2='900' /></g><path d='M 450 225 Q 750 225 750 585' stroke='black' stroke-width='2' fill='none' marker-end='url(#triangle)' /><path d='M 750 750 Q 750 1275 1035 1275' stroke='black' stroke-width='2' fill='none' marker-end='url(#triangle)' /><rect id='form1' stroke='black' stroke-width='2' x='150' y='150' height='150' width='300' fill='#ffffff' /><text transform='translate(162.5 162.5)' x='0' y='0' style='white-space: pre;' font-family='monospace' ><tspan x='0' dy='1rem' font-size='larger' font-weight='bold'>form1</tspan><tspan x='0' dy='1rem'>==========</tspan><tspan x='0' dy='1rem'>Test Form</tspan></text><rect id='cmd1' stroke='black' stroke-width='2' x='600' y='600' height='150' width='300' fill='#60b3f7' /><text transform='translate(612.5 612.5)' x='0' y='0' style='white-space: pre;' font-family='monospace' ><tspan x='0' dy='1rem' font-size='larger' font-weight='bold'>cmd1</tspan><tspan x='0' dy='1rem'>==========</tspan><tspan x='0' dy='1rem'>Test Cmd</tspan></text><rect id='evt1' stroke='black' stroke-width='2' x='1050' y='1200' height='150' width='300' fill='#f7a660' /><text transform='translate(1062.5 1212.5)' x='0' y='0' style='white-space: pre;' font-family='monospace' ><tspan x='0' dy='1rem' font-size='larger' font-weight='bold'>evt1</tspan><tspan x='0' dy='1rem'>==========</tspan><tspan x='0' dy='1rem'>Test Event</tspan><tspan x='0' dy='1rem'>+ foo: str</tspan><tspan x='0' dy='1rem'>+ bar: str</tspan></text></svg>".to_string();
         let mut doc = SvgDocument {
             id: newid(),
             width: 1000f64,
@@ -161,6 +159,7 @@ mod tests {
         };
         doc.set_dimensions(&config);
         let observed = doc.render();
+
         assert_eq!(expected, observed);
     }
 }
