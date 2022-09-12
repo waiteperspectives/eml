b5172cc4a6661ea214476b8848be8c1778ab40fd
Zach Waite <zach@waiteperspectives.com>
2022-09-12 05:23:39 -0400

    [IMP] eml: Add command line interface
    Accepts either a filename or stdin

--- rust/eml/src/main.rs
+++ rust/eml/src/main.rs
@@ -4,8 +4,11 @@ mod parse;
 mod svg;
 mod utils;
 
+use clap::Parser;
 use indoc::indoc;
 use parse::parse;
+use std::fs::File;
+use std::io;
 use svg::*;
 use utils::newid;
 
@@ -102,6 +105,49 @@ fn demo() {
     println!("{}", svg_string);
 }
 
-fn main() {
-    demo()
+fn process(input: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let mut doc = SvgDocument {
+        id: newid(),
+        width: 1000f64,
+        height: 1000f64,
+        cards: Vec::new(),
+        arrows: Vec::new(),
+        swimlane: Swimlane::new(),
+    };
+    let model = parse(input)?;
+    doc.ingest_expressions(model.expressions);
+    let config = SvgConfig {
+        pad: 150f64,
+        card_width: 300f64,
+        card_height: 150f64,
+    };
+    doc.set_dimensions(&config);
+    let svg_string = doc.render();
+    Ok(svg_string)
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// eml input or filepath
+    #[clap(value_parser, default_value = "-")]
+    input: String,
+}
+
+fn read_input(input: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let mut buf = String::new();
+    let mut rdr: Box<dyn io::Read> = match input {
+        "-" => Box::new(io::stdin()),
+        _ => Box::new(File::open(input)?),
+    };
+    rdr.read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let input = read_input(args.input.as_str())?;
+    let output = process(&input)?;
+    println!("{}", output);
+    Ok(())
 }
